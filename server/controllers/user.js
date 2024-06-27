dde70e24056dc697870f3ab66d157bd041b17b6d
karan chaudhary <karanchy95@gmail.com>
2024-06-27 07:48:01 +0545

    user routes added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,8 +1,37 @@
 
-export const test =(req,res)=>{
+export const update =(req,res,next)=>{
 
-    res.json ("its successfull")
-    console.log("test is working")
 }
+
+
+export const deleteUser =(req,res,next)=>{
+
+}
+
+
+export const getUser =(req,res,next)=>{
+
+}
+
+
+export const subscriber =(req,res,next)=>{
+
+}
+
+export const unsubscribe =(req,res,next)=>{
+
+}
+
+
+export const like =(req,res,next)=>{
+
+}
+
+
+export const dislike =(req,res,next)=>{
+
+}
+
+
 
  
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,32 @@
 import express from 'express';
-import { test } from '../controllers/user.js';
+import { deleteUser, getUser, test, update, } from '../controllers/user.js';
 
 
 const router = express.Router();
 
+// update user
+router.put("/:id", update);
 
-router.get("/test",test)
+// delete user
+router.delete("/:id", deleteUser);
+
+
+// get user
+router.get("/find/:id",getUser);
+
+// subscribe a user
+ router.put("/sub/:id")
+
+ // unsubscribe a user
+ router.put("/unsub/:id")
+
+
+ // like a video
+ router.put("/like/:Videoid")
+
+
+ // dislike a video
+ router.put("/dislike/:Videoid")
 
 
 export default router;
